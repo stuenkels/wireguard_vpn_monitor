ba757a4deb69ef5acf29cc7d461f2ec01db277cf
stuenkels <eli.stuenkel@gmail.com>
2025-08-29 11:50:33 -0700

    Dynamically updates page and adds chart

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import psutil 
 import subprocess
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template("index.html")
+
+@app.route('/stats')
+def stats():
     cpu_percent = psutil.cpu_percent(interval=1)
     mem_percent = psutil.virtual_memory().percent
     wg_interface = subprocess.check_output(["wg", "show", "interfaces"]).decode().strip()
@@ -18,19 +22,15 @@ def index():
     rx_bytes = interface_output_json[0]["stats64"]["rx"]["bytes"]
     tx_bytes = interface_output_json[0]["stats64"]["tx"]["bytes"]
 
-    message = None
-    if cpu_percent > 80:
-        message = "High CPU Usage"
-    elif mem_percent > 80:
-        message = "High Memory Usage"    
-    return render_template("index.html",
-        cpu_percent=cpu_percent,
-        mem_percent=mem_percent,
-        wg_interface=wg_interface,
-        rx_bytes = rx_bytes,
-        tx_bytes = tx_bytes,
-        message=message
-    )
+
+    
+    return jsonify({
+        "cpu_percent": cpu_percent,
+        "mem_percent": mem_percent,
+        "wg_interface": wg_interface,
+        "rx_bytes": rx_bytes,
+        "tx_bytes": tx_bytes,
+    })
 
 if __name__ == '__main__':
-    app.run(debug=False, host='10.0.0.1')
+    app.run(debug=False, host='0.0.0.0')
